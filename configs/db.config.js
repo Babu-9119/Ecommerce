9c87b80a1925b3f824af4a7c098fd67323e0f32f
Babu-9119 <babureddypyayala@gmail.com>
2022-05-27 21:09:20 +0530

    added server.js and db config files

---
+++ configs/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: "127.0.0.1",
+    USER: "root",
+    PASSWORD: "Babu@9119",
+    DB: "ecom_db",
+    dialect:"mysql"   //RDBMS being used
+}
---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const serverConfig = require('./configs/server.config');
+const app = express();
+
+app.listen(serverConfig.PORT, () =>{
+    console.log(`server started on PORT: ${serverConfig.PORT}`)
+});
